cfe579bd916d6beaee51e20ccdb5ba58985d99c2
ljlin <lijianlin1995@gmail.com>
2014-06-17 23:24:00 +0800

    ...

--- PlaneWarSwift/GameScene.swift
+++ PlaneWarSwift/GameScene.swift
@@ -25,9 +25,10 @@ class GameScene: SKScene {
 
     override func didMoveToView(view: SKView) {
         
-        self.setUpPlayer();
+        self.setUpPlayer()
+        self.setUpEnemies()
         
-        background.setScale(0.8);
+        background.setScale(0.8)
         background.anchorPoint = CGPointMake(0.5,0.5)
         background.position = CGPointMake(self.size.width / 2 , self.size.height / 2)
         self.addChild(background)
@@ -42,7 +43,8 @@ class GameScene: SKScene {
         }
     }
     override func update(currentTime:CFTimeInterval) {
-        self.setUpOneBullet()
+        //self.setUpOneBullet()
+        self.addEnemies()
     }
     func setUpPlayer() {
         player = PlayerSprite.newPlayerAtPostion(CGPointMake(CGRectGetMidX(self.frame),50))
@@ -53,7 +55,7 @@ class GameScene: SKScene {
     var bullet_setup_count = 0
     func setUpOneBullet() {
         if (bullet_setup_count >= BULLET_RATE) {
-            bullet_setup_count = 0;
+            bullet_setup_count = 0
         } else {
             bullet_setup_count++
             return
@@ -66,6 +68,22 @@ class GameScene: SKScene {
         var action = SKAction.moveTo(dest,duration:time)
         bullet.runAction(action,completion:{bullet.removeFromParent()})
     }
+    func setUpEnemies() {
+        for i in 0..ENEMIES_MAX_COUNT {
+            var sprite : EnemySprite?
+            if (i % ENEMY_LARGE_RATE == 0) {
+                sprite = EnemySprite.newEnemyWithEnemyType(EnemyTypeLarge)
+            } else if (i % ENEMY_MIDDIUM_RATE == 0) {
+                sprite = EnemySprite.newEnemyWithEnemyType(EnemyTypeMiddium)
+            } else {
+                sprite = EnemySprite.newEnemyWithEnemyType(EnemyTypeSmall)
+            }
+            if (sprite?.parent != self) {
+                enemiesArray += sprite
+            }
+        }
+    }
+
     func availabelSprite() -> EnemySprite? {
         let rand_count = random() % enemiesArray.count
         var sprite :EnemySprite? = enemiesArray[rand_count]
@@ -82,12 +100,12 @@ class GameScene: SKScene {
         if let sprite = self.availabelSprite(){
         if (random() % 77 == 0) {
             let x :Float = Float(random() % 1000) * 0.1
-            //let x = random() % 1000 * ((CGRectGetMaxX(self.frame) - sprite.size.width ) / 1000) + sprite.size.width / 2;
-            let position = CGPointMake(x, CGRectGetMaxY(self.frame) + sprite.size.height);
+            //let x = random() % 1000 * ((CGRectGetMaxX(self.frame) - sprite.size.width ) / 1000) + sprite.size.width / 2
+            let position = CGPointMake(x, CGRectGetMaxY(self.frame) + sprite.size.height)
             player.position = position
-            let dest = CGPointMake(x, -sprite.size.height);
+            let dest = CGPointMake(x, -sprite.size.height)
             self.addChild(sprite)
-            let time = Double(fabs(Double(dest.y) - Double(position.y))) / Double(sprite.speed);
+            let time = Double(fabs(Double(dest.y) - Double(position.y))) / Double(sprite.speed)
             let action = SKAction.moveTo(dest,duration:time)
             sprite.runAction(action,completion:{
                 sprite.removeFromParent()
